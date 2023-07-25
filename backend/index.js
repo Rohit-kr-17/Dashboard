d19a62563adabdfd2d7742d61612d05deb83e94a
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2023-07-26 03:53:01 +0530

    deployed

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const path = require("path");
 require("express-async-errors");
 require("./db");
 
@@ -12,13 +11,8 @@ const { errorHandler } = require("./middlewares/error");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use("/user", userRouter);
 app.use("/product", productRouter);
-
-app.use("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
 app.use(errorHandler);
 
 app.listen(8000, () => {

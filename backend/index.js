cb05235d0318ff2943cd948d4221025730444675
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2023-07-26 03:21:46 +0530

    react build

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 require("express-async-errors");
 require("./db");
 
@@ -11,8 +12,13 @@ const { errorHandler } = require("./middlewares/error");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use("/user", userRouter);
 app.use("/product", productRouter);
+
+app.use("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 app.use(errorHandler);
 
 app.listen(8000, () => {

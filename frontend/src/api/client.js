e41ecea9e16574251dc75758cab72792af51312e
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2023-07-26 21:40:50 +0530

    updated client url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,4 +1,6 @@
 import axios from "axios";
-const client = axios.create({ baseURL: "http://localhost:8000/api" });
+const client = axios.create({
+	baseURL: "https://productdashboard-qp9e.onrender.com/api",
+});
 
 export default client;
